42810acf4142f22040ea47a896c21ca85a99c708
Pawel Reich <thegitman@outlook.com>
2023-02-22 16:07:50 +0100

    Create task.h

---
+++ src/kernel/task/task.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "paging/paging.h"
+
+struct registers
+{
+    uint32_t edi;
+    uint32_t esi;
+    uint32_t ebp;
+    uint32_t ebx;
+    uint32_t edx;
+    uint32_t ecx;
+    uint32_t eax;
+
+    uint32_t ip;
+    uint32_t cs;
+    uint32_t flags;
+    uint32_t esp;
+    uint32_t ss;
+};
+
+struct task 
+{
+    struct paging_4gb_chunk* page_directory;
+}

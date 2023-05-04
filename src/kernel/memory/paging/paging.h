022920a462ca5f48025899b0cc25ce79c42b1572
Pawel Reich <thegitman@outlook.com>
2023-05-04 15:48:39 +0200

    Update paging.h

--- src/kernel/memory/paging/paging.h
+++ src/kernel/memory/paging/paging.h
@@ -24,3 +24,4 @@ int paging_set_page(uint32_t* directory, void* virtual_address, uint32_t value);
 int paging_map_range(uint32_t* directory, void* virtual, void* physical, int count, int flags);
 int paging_map_to(uint32_t* directory, void* virtual, void* physical, void* physical_end, int flags);
 int paging_map(uint32_t* directory, void* virtual, void* physical, int flags);
+void* paging_align_address(void* ptr);

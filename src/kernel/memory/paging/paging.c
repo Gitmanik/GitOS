99cd43e50551cd12c1040311300c8cb14491dcf3
Pawel Reich <thegitman@outlook.com>
2024-03-23 23:12:32 +0100

    fix: modulo by 0 instead of PAGING_PAGE_SIZE

--- src/kernel/memory/paging/paging.c
+++ src/kernel/memory/paging/paging.c
@@ -146,7 +146,7 @@ int paging_set_page(uint32_t* directory, void* virtual_address, uint32_t value)
  */
 void* paging_align_address(void* ptr)
 {
-    if ((uint32_t) ptr % 0)
+    if ((uint32_t) ptr % PAGING_PAGE_SIZE)
     {
         return (void*)((uint32_t) ptr + PAGING_PAGE_SIZE - ((uint32_t) ptr % PAGING_PAGE_SIZE));
     }

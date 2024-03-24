d93d24de078e93edc04b2d543e7c096fa5790d0f
Pawel Reich <thegitman@outlook.com>
2024-03-24 22:49:54 +0100

    chore: Removed redundant cli instructions

--- src/kernel/idt/idt_handler.asm
+++ src/kernel/idt/idt_handler.asm
@@ -3,18 +3,14 @@ extern ignore_int_handler
 
 global int21h
 int21h:
-    cli
     pushad
     call int21h_handler
     popad
-    sti
     iret
 
 global ignore_int
 ignore_int:
-    cli
     pushad
     call ignore_int_handler
     popad
-    sti
     iret

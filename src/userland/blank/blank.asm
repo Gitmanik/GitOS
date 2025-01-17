7df0513a4f17d9fa2a662d850b8d98c218872dae
Paweł Reich <37516105+Gitmanik@users.noreply.github.com>
2025-01-17 13:14:02 +0100

    userland blank: Now truly blank(tm)

--- src/userland/blank/blank.asm
+++ src/userland/blank/blank.asm
@@ -1,23 +1,6 @@
 [BITS 32]
 
-extern getc, putc, puts
-global main
+extern main
 main:
-    push str
-    call puts
-
-loop:
-    call waitforkey
-    push eax
-    call putc
-    jmp loop
-
-waitforkey:
-    call getc
-    cmp eax, 0x00
-    je waitforkey
+    mov eax, 0
     ret
-
-section .data
-str:
-    db 'Hello from usermode! Press a key to show it on screen!', 0

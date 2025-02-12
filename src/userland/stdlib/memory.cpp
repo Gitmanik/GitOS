87b50f8c668dfa2e3b51e2d15c59b70d3bfa4c99
Paweł Reich <37516105+Gitmanik@users.noreply.github.com>
2025-02-12 18:45:08 +0100

    U: C++ new/delete operators, atexit

---
+++ src/userland/stdlib/memory.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Paweł Reich on 2/12/25.
+//
+#include "stddef.h"
+
+extern "C" {
+#include <misc.h>
+#include "string.h"
+}
+
+extern "C" int atexit(void (*)())
+{
+    return 0;
+}
+
+void *operator new(size_t size)
+{
+    return malloc(size);
+}
+
+void *operator new[](size_t size)
+{
+    return malloc(size);
+}
+
+void operator delete(void *p)
+{
+    free(p);
+}
+
+void operator delete[](void *p)
+{
+    free(p);
+}

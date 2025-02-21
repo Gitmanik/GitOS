4bc02e8192faa9c917436c413e3f7693f9872bf0
Paweł Reich <37516105+Gitmanik@users.noreply.github.com>
2025-02-21 01:27:06 +0100

    Write debug serial output to scripts/serial.log

--- scripts/qemu_debug.sh
+++ scripts/qemu_debug.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env sh
-qemu-system-i386 -drive file=/IdeaProjects/GitOS/cmake-build-debug/disk.bin,format=raw,index=0,media=disk -m 32M -S -gdb tcp::1234 &
+qemu-system-i386 -drive file=/IdeaProjects/GitOS/cmake-build-debug/disk.bin,format=raw,index=0,media=disk \
+                 -m 32M \
+                 -chardev stdio,id=char0,logfile=serial.log,signal=off \
+                 -serial chardev:char0 \
+                 -gdb tcp::1234 &
